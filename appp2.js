2b61d1b4f472264824378deb798d769441377219
Ankit Singh <154090199+singhsinghel@users.noreply.github.com>
2024-03-11 12:07:12 +0530

    Update appp2.js
    Made a function for ketdown and button click

--- appp2.js
+++ appp2.js
@@ -8,66 +8,41 @@ del.classList.add('add');
 del.classList.add('remove');
 main.appendChild(del);
 
-button.addEventListener('click',function(){
+function events(){
+    let li=document.createElement('li');   //creating a list for ul
+    li.innerText=input.value;
+
+    let btn=document.createElement('button');   // creating a delete button to delete a list
+    btn.innerText="";
+    btn.classList.add("delete");
+
+    li.appendChild(btn);     // appending button in the list
+    text.appendChild(li);   // appending list in the ul
+
+
+    input.value='';
+    text.style.display='block';      
+    btn.addEventListener('click',function(){
+    this.parentElement.classList.add('new');
+    this.style.backgroundColor="rgb(207, 102, 102)";
+
+    del.addEventListener('click',function(){
+    li.remove();
+      });
+  });
+}
+function checkEmpty(){
     if (input.value.trim()=="")
     alert("your task is empty!!");
-    
-    else{
-      let li=document.createElement('li');   //creating a list for ul
-      li.innerText=input.value;
-
-      let btn=document.createElement('button');   // creating a delete button to delete a list
-      btn.innerText="";
-      btn.classList.add("delete");
-
-      li.appendChild(btn);     // appending button in the list
-       text.appendChild(li);   // appending list in the ul
-
-
-      input.value='';
-      text.style.display='block';      
-      btn.addEventListener('click',function(){
-        this.parentElement.classList.add('new');
-        this.style.backgroundColor="rgb(207, 102, 102)";
-
-        del.addEventListener('click',function(){
-            li.remove();
-        });
-
-    });
-    
-   
+    else  events();
+}
+button.addEventListener('click',function(){
+    checkEmpty();    
     }
+);
 
-
-});
 input.addEventListener('keydown',function(event){
-    
     if(event.code=='Enter'){
-        if (input.value.trim()=="")
-        alert("your task is empty!!");
-
-     else{
-         let li=document.createElement('li');
-         li.innerText=input.value;
-         let btn=document.createElement('button');
-         btn.classList.add("delete");
-         btn.innerText="";
-         
-         li.appendChild(btn);
-         text.appendChild(li);
-       
-         input.value='';
-         text.style.display='block';
-
-         btn.addEventListener('click',function(){
-            this.parentElement.classList.add('new');
-         this.style.backgroundColor="rgb(207, 102, 102)"
-         });
-         del.addEventListener('click',function(){
-            li.remove();
-        });
-      
-        }
-    }
-});
+     ncheckEmpty();
+    }}
+);
